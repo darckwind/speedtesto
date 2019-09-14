fd28b177e07c2560042f5bd38d23af0b99717332
Riko <f.bunout01@ufromail.cl>
2019-09-13 23:38:02 -0300

    add ping to server test

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 import speedtest as sp
-import json
+import json, subprocess
 
 def cancer():
+    # start services of speedtest
     narnia = sp.Speedtest()
+    # fix server
     server_data = [{'url': 'http://dallas02.speedtest.windstream.net/speedtest/upload.php', 'lat': '32.7766', 'lon': '-96.7969', 'name': 'Dallas, TX', 'country': 'United States', 'cc': 'US', 'sponsor': 'Windstream', 'id': '17386', 'host': 'dallas02.speedtest.windstream.net:8080', 'd': 7855.541337725319}]
     narnia.get_best_server(server_data)
+    #format json of responce
     responce = {
         "SpeedTestUpload":narnia.upload()/1e+6,
         "SpeedTestDownload":narnia.download()/1e+6,
         "SpeedTestLatency":narnia.results.ping,
+        "PingServer":ping_real()
     }
     return(responce)
+def ping_real():
+    res_con = []
+    proc = subprocess.Popen(['ping','45.79.49.144'],universal_newlines=6,stdout=subprocess.PIPE)
+    while True:
+        line = proc.stdout.readline()
+        if not line:
+            break
+        res_con.append(line.rstrip())
+
+    res_con = res_con[: len(res_con) - 6]
+    res_con.pop(0)
+    res_con.pop(0)
+    return res_con
 
 def main():
     with open('Speedtest.json', 'w') as outfile:
