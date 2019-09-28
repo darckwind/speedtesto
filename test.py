38891584c0c638dfa6369b9ef078b8fe265cfaf7
Riko <f.bunout01@ufromail.cl>
2019-09-27 22:21:10 -0300

    minimal updates

--- test.py
+++ test.py
@@ -1,34 +1,28 @@
 import speedtest as sp
 import json, subprocess, os, sys,getopt
 
+
 def cancer():
     # start services of speedtest
-    narnia = sp.Speedtest()
+    import os, ssl
+
+    if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+        getattr(ssl, '_create_unverified_context', None)):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+        narnia = sp.Speedtest()
     # fix server
     server_data = [{'url': 'http://dallas02.speedtest.windstream.net/speedtest/upload.php', 'lat': '32.7766', 'lon': '-96.7969', 'name': 'Dallas, TX', 'country': 'United States', 'cc': 'US', 'sponsor': 'Windstream', 'id': '17386', 'host': 'dallas02.speedtest.windstream.net:8080', 'd': 7855.541337725319}]
     narnia.get_best_server(server_data)
+
     #format json of responce
     responce = {
         "SpeedTestUpload":narnia.upload()/1e+6,
         "SpeedTestDownload":narnia.download()/1e+6,
         "SpeedTestLatency":narnia.results.ping,
-        "PingServer":ping_real()
+        "PingServer":0.0
     }
     return(responce)
-def ping_real():
-    res_con = []
-    proc = subprocess.Popen(['ping','45.79.49.144'],universal_newlines=6,stdout=subprocess.PIPE)
-    while True:
-        line = proc.stdout.readline()
-        if not line:
-            break
-        res_con.append(line.rstrip())
-
-    res_con = res_con[: len(res_con) - 6]
-    res_con.pop(0)
-    res_con.pop(0)
-    return res_con
-
 def main(argv):
     outputfile=''
     try:
@@ -42,9 +36,18 @@ def main(argv):
             sys.exit()
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-    name = str(outputfile+"/Speedtest.json")
+    if str(outputfile) == '':
+        name="Speedtest.json"
+    else:
+        name = str(outputfile+"/Speedtest.json")
+    
+    with open(name, 'w') as outfile:
+        json.dump({"SpeedTestUpload":0.00,"SpeedTestDownload":0.00,"SpeedTestLatency":0.00,"PingServer":0.00}, outfile)
+
     with open(name, 'w') as outfile:
         json.dump(cancer(), outfile)
+
+    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
